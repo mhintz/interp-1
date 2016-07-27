984759e8b970b5bf557d0274031e757c37cb7bc5
Mark Hintz <markohintz@gmail.com>
2016-07-27 16:18:47 +0200

    Reduce the scale factor of the dot value, so that color transitions are a little slower

--- resources/facetShader_fs.glsl
+++ resources/facetShader_fs.glsl
@@ -13,7 +13,7 @@ void main() {
   vec3 normal = normalize(fNormal);
 
   float dotValue = abs(dot(normal, toCamera));
-  dotValue = fract(dotValue * 10.0);
+  dotValue = fract(dotValue * 3.0);
 
   // vec3 partialColor = fract(fColor.xyz + vec3(dotValue / 3.0, dotValue / 2.0, dotValue / 1.0));
   vec3 partialColor = fract(fColor.xyz + vec3(dotValue));
