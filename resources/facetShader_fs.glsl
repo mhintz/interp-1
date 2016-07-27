09cb27fae6b6ac06c78cefb93f0efe7faec10d40
Mark Hintz <markohintz@gmail.com>
2016-07-27 16:10:55 +0200

    rebase the dot value a bit, increase range, make it a little random

--- resources/facetShader_fs.glsl
+++ resources/facetShader_fs.glsl
@@ -12,9 +12,12 @@ void main() {
   vec3 toCamera = normalize(uCameraPosition - fWorldPosition);
   vec3 normal = normalize(fNormal);
 
-  float dotValue = abs(dot(normal, toCamera));
+  float dotValue = dot(normal, toCamera) / 2.0 + 0.5;
+  dotValue = fract(dotValue * 5.314151);
 
-  vec3 partialColor = fract(fColor.xyz + vec3(dotValue / 3.0, dotValue / 2.0, dotValue / 1.0));
+  // vec3 partialColor = fract(fColor.xyz + vec3(dotValue / 3.0, dotValue / 2.0, dotValue / 1.0));
+  // vec3 partialColor = fract(fColor.xyz + vec3(dotValue));
+  vec3 partialColor = vec3(dotValue);
 
   FragColor = vec4(partialColor, fColor.a);
 }
